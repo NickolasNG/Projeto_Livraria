e211ff388ea8cff1fc0a0857b35ccdcc99a4c923
Destroier007 <nickolas.ng@hotmail.com>
2023-09-25 10:47:19 -0300

    Evoluindo a API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from "express";
-import conectaNaDataBase from "./config/dbConnect.js";
-import livro from "./models/Livro.js";
+import conectaDataBase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
 
-const conexao = await conectaNaDataBase();
+const conexao = await conectaDataBase();
 // ormalmente, em JavaScript, os métodos que têm o nome on estão relacionados a algum evento
 conexao.on("error", (erro) => {
     console.error("erro de conexao", erro);
@@ -13,42 +13,7 @@ conexao.once("open", () => {
 });
 
 const app = express();
-app.use(express.json());
-
-// CRUD - Criar, Ler, atualizar, deletar
-
-// função que busca o livro em meio a lista por ID
-app.get("/", (req, res) => {
-    res.status(200).send("curso de Node.js");
-});
-// LER os daddos do banco
-app.get("/livros", async (req, res) => {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
-});
-// LER os daddos do banco
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id)
-    res.status(200).json(livros[index]);
-});
-
-// CREATE, criar novos dados no banco 
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("livro cadastrado com sucesso");
-});
-// ATUALIZAR os dados do banco
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros);
-});
-// deletar 
-app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("livro removido com sucesso");
-});
+routes(app);
 
 export default app;
 
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-async function conectaNaDataBase(){
+async function conectaDataBase() {
     mongoose.connect(process.env.DB_CONNECTION_STRING);
-
     return mongoose.connection;
 };
 
-export default conectaNaDataBase;
+
+export default conectaDataBase;
 
