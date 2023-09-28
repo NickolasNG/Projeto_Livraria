263ebcbea59c98b57949a963fc4eb150a73b69c4
Destroier007 <nickolas.ng@hotmail.com>
2023-09-28 11:15:36 -0300

    save

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,18 +1,11 @@
 import mongoose from "mongoose";
 
 // eslint-disable-next-line no-unused-vars
-function manipuladorDeErros (erro, req, res, next){
+function manipuladorDeErros(erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError){
-    res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
-  } else if (erro instanceof mongoose.Error.ValidationError){
-    console.log(erro.errors);
-
-    const mensagensErro = Object.values(erro.errors)
-      .map(erro => erro.message)
-      .join("; ");
-    res(400).send({message: `Os seguintes erros foram encontrados: ${mensagensErro}`});
+    res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos"});
   } else {
-    res.status(500).send({message: "Erro interno do servidor."});
+    res.status(500).send({message: "Erro interno de servidor"});
   }
 }
 
